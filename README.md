358900df0242787a49082d18309d77e1a921f00b
Charles Shi <schrht@gmail.com>
2022-09-12 10:42:24 -0400

    Add tmt-beaker.sh to trigger RHEL verification.

--- README.md
+++ README.md
@@ -64,3 +64,25 @@ Notes:
 ## tmt-vm.sh
 
 This script help to start a RHIVOS VM through QEMU. Notes: Update hardcoded VARIABLEs before using.
+
+# tmt-beaker.sh
+
+```
+Description:
+  Trigger beaker jobs for verifiying code changes on RHEL.
+Usage:
+  tmt-beaker.sh <-f HOSTFILTER> <-d DISTRO> <-a ARCH> <-r REPOSITORY> <-b BRANCH> <-p PATH>
+  - HOSTFILTER: Filter name defined in ~/.beaker_client/host-filter
+  - DISTRO    : RHEL-9.1.0
+  - ARCH      : x86_64, ppc64le, s390x, aarch64
+  - REPOSITORY: public, private
+  - BRANCH    : main, mr_branch_name
+  - PATH      : Ex. rt-tests/env_test
+Available HOSTFILTER(s):
+  INTEL_6_79_1 INTEL_6_63_2 INTEL_6_85_4 AMD_23 INTEL_6_60_3 INTEL_6_58_9 INTEL_6_44_2 INTEL_6_42_7 INTEL_6_30_5 AMD INTEL ICELAKE CASCADELAKE KABYLAKE SKYLAKE BROADWELL HASWELL1 HASWELL2 IVY1 IVY2 SANDY1 SANDY2 RHEL8 RHEL6VM RHEL7VM RHEL8VM RHEL9VM THUNDERX_2 HUAWEI_D05 HUAWEI_D06 NVME ROME MILAN WIRELESS NOGSS NOGSS_MEMORY_MIN_16G RHEL7z RHEL8z RHEL83z RHELRT INTEL_RT AMD_RT IBM__POWER9_NUMAGT_1 INTEL__ICELAKE_NUMAGT_1 TRACEBLACKLIST RTS_NUMA2_VM
+Example:
+  tmt-beaker.sh -h INTEL -d RHEL-9.1.0 -a x86_64 -r private -b master -p rt-tests/env_test
+Notes:
+  1. Follow https://docs.engineering.redhat.com/display/Automotive/Configure+beaker+client to setup;
+  2. Update hardcoded VARIABLEs before using.
+```
---
+++ tmt-beaker.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Desciption: Trigger beaker jobs for verifiying code changes on RHEL.
+# Owner: Charles Shi <cheshi@redhat.com>
+
+# Update the following variables before use
+DEFAULT_HOSTFILTER=INTEL
+DEFAULT_DISTRO=RHEL-9.1.0
+DEFAULT_ARCH=x86_64
+PUBLIC_REPO_URL=https://gitlab.com/schrht/kernel-tests
+PRIVATE_REPO_URL=https://gitlab.cee.redhat.com/cheshi/kernel
+
+# Function
+function show_usage() {
+  echo "Description:"
+  echo "  Trigger beaker jobs for verifiying code changes on RHEL."
+  echo "Usage:"
+  echo "  $(basename $0) <-f HOSTFILTER> <-d DISTRO> <-a ARCH> <-r REPOSITORY> <-b BRANCH> <-p PATH>"
+  echo "  - HOSTFILTER: Filter name defined in ~/.beaker_client/host-filter"
+  echo "  - DISTRO    : RHEL-9.1.0"
+  echo "  - ARCH      : x86_64, ppc64le, s390x, aarch64"
+  echo "  - REPOSITORY: public, private"
+  echo "  - BRANCH    : main, mr_branch_name"
+  echo "  - PATH      : Ex. rt-tests/env_test"
+  echo "Available HOSTFILTER(s):"
+  echo "  $(cat ~/.beaker_client/host-filter | awk '{print $1}' | xargs)"
+  echo "Example:"
+  echo "  $(basename $0) -h INTEL -d RHEL-9.1.0 -a x86_64 -r private -b master -p rt-tests/env_test"
+  echo "Notes:"
+  echo "  1. Follow https://docs.engineering.redhat.com/display/Automotive/Configure+beaker+client to setup;"
+  echo "  2. Update hardcoded VARIABLEs before using."
+}
+
+function verify_task() {
+  echo "Verifing $verify_url ..."
+
+  if (curl $verify_url/metadata 2>/dev/null | grep -q 'automotive/include' ||
+    curl $verify_url/runtest.sh 2>/dev/null | grep -q 'kernel_automotive'); then
+    echo "Verified $path: SUCCESS (PATH EXISTS AND AUTOMOTIVE CODE FOUND)"
+    return 0
+  fi
+
+  if (curl $verify_url/metadata 2>/dev/null | grep -q 'restraint' ||
+    curl $verify_url/Makefile 2>/dev/null | grep -q '^run:' ||
+    curl $verify_url/runtest.sh 2>/dev/null | grep -q '^#!/bin/bash'); then
+    echo "Verified $path: FAIL (PATH EXISTS BUT AUTOMOTIVE CODE NOT FOUND)"
+    return 1
+  else
+    echo "Verified $path: FAIL (PATH NOT FOUND)"
+    return 2
+  fi
+}
+
+while getopts :hf:d:a:r:b:p: ARGS; do
+  case $ARGS in
+  h)
+    # Help option
+    show_usage
+    exit 0
+    ;;
+  f)
+    # HOSTFILTER option
+    hostfilter=$OPTARG
+    ;;
+  d)
+    # DISTRO option
+    distro=$OPTARG
+    ;;
+  a)
+    # ARCH option
+    arch=$OPTARG
+    ;;
+  r)
+    # REPOSITORY option
+    repository=$OPTARG
+    ;;
+  b)
+    # BRANCH option
+    branch=$OPTARG
+    ;;
+  p)
+    # PATH option
+    path=$OPTARG
+    ;;
+  "?")
+    echo "$(basename $0): unknown option: $OPTARG" >&2
+    ;;
+  ":")
+    echo "$(basename $0): option requires an argument -- '$OPTARG'" >&2
+    echo "Try '$(basename $0) -h' for more information." >&2
+    exit 1
+    ;;
+  *)
+    # Unexpected errors
+    echo "$(basename $0): unexpected error -- $ARGS" >&2
+    echo "Try '$(basename $0) -h' for more information." >&2
+    exit 1
+    ;;
+  esac
+done
+
+[ -z "$hostfilter" ] && hostfilter=$DEFAULT_HOSTFILTER
+[ -z "$distro" ] && distro=$DEFAULT_DISTRO
+[ -z "$arch" ] && arch=$DEFAULT_ARCH
+[ -z "$repository" ] && repository=public
+
+if [ -z "$path" ]; then
+  show_usage
+  exit 1
+fi
+
+case $repository in
+public)
+  [ -z "$branch" ] && branch=main
+  task_url=$PUBLIC_REPO_URL/-/archive/$branch/kernel-$branch.tar.gz#$path
+  verify_url=$PUBLIC_REPO_URL/-/raw/$branch/$path
+  ;;
+private)
+  [ -z "$branch" ] && branch=master
+  task_url=$PRIVATE_REPO_URL/-/archive/$branch/kernel-$branch.tar.gz#$path
+  verify_url=$PRIVATE_REPO_URL/-/raw/$branch/$path
+  ;;
+*)
+  echo "$(basename $0): unexpected REPOSITORY." >&2
+  show_usage
+  exit 1
+  ;;
+esac
+
+whiteboard="$(basename $0) $path $distro ($arch)"
+
+cmd="bkr workflow-tomorrow --restraint --distro $distro --arch $arch \
+  --systype Machine --host-filter $hostfilter --crb --ignore-panic \
+  --ks-meta redhat_ca_cert --whiteboard \"$whiteboard\" \
+  --task $task_url"
+
+if verify_task; then
+  echo -e "\nBeaker command:\n$cmd"
+  exit 0
+else
+  exit 1
+fi
